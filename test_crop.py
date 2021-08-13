549dd43242f14f8aa84bb7ac47042788fbe495b4
ppypn4 <ppypm4@nottingham.ac.uk>
2021-08-13 15:58:16 +0100

    changed variable names

--- test_crop.py
+++ test_crop.py
@@ -17,59 +17,31 @@ def trim_video(vid, start, stop):
     clip.write_videofile("cropped_vid.mp4")
 
 
-# def combine_boxes(video):
-#     cap = cv2.VideoCapture(video)
-#     i = 0
-#     im1 = np.zeros((1270, 1450), dtype=int)
-#     combined = im1
-#     white_pixel_percentage = np.array([])
-#
-#     while cap.isOpened():
-#         ret, frame = cap.read()
-#         if ret == False:
-#             break
-#
-#         im2 = format_im(frame)
-#         both = im2 * im1
-#         both = np.uint8(both / np.max(both) * 255)
-#         combined += both
-#         combined = images.threshold(combined / 255, 0.9)
-#         images.display(combined)
-#         no_white_pixels = np.sum(combined == 255)
-#         percentage = no_white_pixels/combined.size
-#         white_pixel_percentage = np.append(white_pixel_percentage, percentage)
-#         im1 = im2
-#         i += 1
-#
-#     cap.release()
-#     cv2.destroyAllWindows()
-
-
 def crop_video_into_grid(im, rows, cols):
-    imwidth, imheight = im.shape
-    box_height = int(imheight / rows)
-    box_width = int(imwidth / cols)
-    for i in range(0, imheight - box_height, box_height):
-        for j in range(0, imwidth - box_width, box_width):
+    im_width, im_height = im.shape
+    box_height = int(im_height / rows)
+    box_width = int(im_width / cols)
+    for i in range(0, im_height - box_height, box_height):
+        for j in range(0, im_width - box_width, box_width):
             cropped = im[j:j + box_width, i:i + box_height]
             combine_boxes(cropped)
 
 
 def format_video_frame(im, frame_no, rows, cols):
-    imwidth, imheight = im.shape
-    box_height = int(imheight / rows)
-    height_remainder = imheight % rows
+    im_width, im_height = im.shape
+    box_height = int(im_height / rows)
+    height_remainder = im_height % rows
     if height_remainder == 0:
-        imheight += 1
-    box_width = int(imwidth / cols)
-    width_remainder = imwidth % rows
+        im_height += 1
+    box_width = int(im_width / cols)
+    width_remainder = im_width % rows
     if width_remainder == 0:
-        imwidth += 1
+        im_width += 1
     global white_pixel_percentage_array
     a = 0
-    for i in range(0, imheight - box_height, box_height):
+    for i in range(0, im_height - box_height, box_height):
         b = 0
-        for j in range(0, imwidth - box_width, box_width):
+        for j in range(0, im_width - box_width, box_width):
             cropped = im[j:j + box_width, i:i + box_height]
             if white_pixel_percentage_array[a][b] <= 99:
                 no_white_pixels = np.sum(cropped == 255)
@@ -86,7 +58,7 @@ def combine_boxes(video, rows, cols):
     cap = cv2.VideoCapture(video)
     i = 0
     im1 = np.zeros((1270, 1450), dtype=int)
-    combined = im1
+    cumulative_image_combination = im1
     frame_no = 100
 
     while cap.isOpened():
@@ -95,11 +67,11 @@ def combine_boxes(video, rows, cols):
             break
         frame_no += 1
         im2 = format_im(frame)
-        both = im2 * im1
-        both = np.uint8(both / np.max(both) * 255)
-        combined += both
-        combined = images.threshold(combined / 255, 0.9)
-        format_video_frame(combined, frame_no=frame_no, rows=rows, cols=cols)
+        combined_image = im2 * im1
+        combined_image = np.uint8(combined_image / np.max(combined_image) * 255)
+        cumulative_image_combination += combined_image
+        cumulative_image_combination = images.threshold(cumulative_image_combination / 255, 0.9)
+        format_video_frame(cumulative_image_combination, frame_no=frame_no, rows=rows, cols=cols)
         im1 = im2
         i += 1
 
